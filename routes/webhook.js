152efe3da7f9b6948e86cc674d9c7f6d1299863c
Wilian <wilianzilv@gmail.com>
2019-08-16 19:02:14 -0300

    added .env

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,48 +1,50 @@
 const express = require('express')
 const router = express.Router()
+const dotenv = require('dotenv')
+dotenv.config()
 
 router.get('/', async (req, res) => {
-  // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = '2UN22rl7BmgIqKFGGznkYPJdWQhjvCku'
+	// Your verify token. Should be a random string.
+	let VERIFY_TOKEN = process.env.VERIFY_TOKEN
 
-  // Parse the query params
-  let mode = req.query['hub.mode']
-  let token = req.query['hub.verify_token']
-  let challenge = req.query['hub.challenge']
+	// Parse the query params
+	let mode = req.query['hub.mode']
+	let token = req.query['hub.verify_token']
+	let challenge = req.query['hub.challenge']
 
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-    // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED')
-      res.status(200).send(challenge)
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403)
-    }
-  }
+	// Checks if a token and mode is in the query string of the request
+	if (mode && token) {
+		// Checks the mode and token sent is correct
+		if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+			// Responds with the challenge token from the request
+			console.log('WEBHOOK_VERIFIED')
+			res.status(200).send(challenge)
+		} else {
+			// Responds with '403 Forbidden' if verify tokens do not match
+			res.sendStatus(403)
+		}
+	}
 })
 
 router.post('/', (req, res) => {
-  let body = req.body
+	let body = req.body
 
-  // Checks this is an event from a page subscription
-  if (body.object === 'page') {
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
-      // Gets the message. entry.messaging is an array, but
-      // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0]
-      console.log(webhook_event)
-    })
+	// Checks this is an event from a page subscription
+	if (body.object === 'page') {
+		// Iterates over each entry - there may be multiple if batched
+		body.entry.forEach(function(entry) {
+			// Gets the message. entry.messaging is an array, but
+			// will only ever contain one message, so we get index 0
+			let webhook_event = entry.messaging[0]
+			console.log(webhook_event)
+		})
 
-    // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED')
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404)
-  }
+		// Returns a '200 OK' response to all requests
+		res.status(200).send('EVENT_RECEIVED')
+	} else {
+		// Returns a '404 Not Found' if event is not from a page subscription
+		res.sendStatus(404)
+	}
 })
 
 module.exports = app => app.use('/webhook', router)
