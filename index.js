ddef75c6e163f747fcb272e2340d5c3ec202e89f
Wilian <wilianzilv@gmail.com>
2019-08-17 18:29:28 -0300

    update

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static('public'))
 app.use(express.json())
 
 require('./routes/webhook')(app)
 
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'))
 app.listen(80)
